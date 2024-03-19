218290a7e0ddc5632e97e8adaa8a1ad1f9edf914
MsWakesho <wakeshosonja@gmail.com>
2024-03-19 11:12:44 +0300

    completed lab

--- lib/car.py
+++ lib/car.py
@@ -1,4 +1,5 @@
 from vehicle import Vehicle
-
+# Vehicle as an argument for the Car class to note that Car inherits from Vehicle.
 class Car(Vehicle):
-    pass
+    def go(self):
+        return 'VRRROOOOOOOOOOOOOOOOOOOOOOOM!!!!!'
--- lib/vehicle.py
+++ lib/vehicle.py
@@ -1,2 +1,10 @@
 class Vehicle:
-    pass
+    def __init__(self,wheel_size,wheel_number):
+        self.wheel_size= wheel_size
+        self.wheel_number = wheel_number
+# go and fill_up_tank,these are instance methods.
+    def go(self):
+        return "vrrrrrrrooom!"
+    
+    def fill_up_talk(self):
+        return "filling up!"
